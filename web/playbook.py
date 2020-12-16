a4beda9bec7517f5ccb5c8c7d69870bbf49d56b2
Z1P TI3 <z1pti3@outlook.com>
2020-12-16 21:26:43 +0000

    Temp Updated UI

--- web/playbook.py
+++ web/playbook.py
@@ -1,7 +1,10 @@
+import urllib.parse
+from pathlib import Path
 from flask import Blueprint, render_template
 from flask import current_app as app
+from flask import request, send_from_directory
+from markupsafe import Markup
 
-from pathlib import Path
 import time
 
 from core import api
@@ -9,13 +12,40 @@ from plugins.playbook.models import playbook
 
 pluginPages = Blueprint('playbookPages', __name__, template_folder="templates")
 
+@pluginPages.app_template_filter('urlencode')
+def urlencode_filter(s):
+    if type(s) == 'Markup':
+        s = s.unescape()
+    s = s.encode('utf8')
+    s = urllib.parse.quote_plus(s)
+    return Markup(s)
+
+@pluginPages.route('/playbook/includes/<file>')
+def custom_static(file):
+    return send_from_directory(str(Path("plugins/playbook/web/includes")), file)
+
 @pluginPages.route("/playbook/")
 def mainPage():
-    foundPlays = playbook._playbook().query()["results"]
+    foundPlays = playbook._playbook().query(sessionData=api.g.sessionData)["results"]
+    playbooks = []
+    for play in foundPlays:
+        if play["name"] not in playbooks:
+            playbooks.append(play["name"])
+    return render_template("playbooks.html", content=playbooks)
+
+
+@pluginPages.route("/playbook/<playbookName>/")
+def getPlaybookByName(playbookName):
+    foundPlays = playbook._playbook().query(sessionData=api.g.sessionData,query={"name" : playbookName})["results"]
     plays = []
+    pie = {"complete" : 0, "incomplete" : 0}
     for play in foundPlays:
         plays.append(play)
-    return render_template("playbook.html", plays=plays)
+        if play["result"]:
+            pie["complete"] += 1
+        else:
+            pie["incomplete"] += 1
+    return render_template("playbook.html", content=plays, pie=pie, name=playbookName)
 
 
 @pluginPages.route("/playbook/<occurrenceID>/clear/")
