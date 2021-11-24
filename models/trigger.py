a752f7f2d400e7a0ca5e0c4115c1264ff798832c
z1pti3 <z1pti3@outlook.com>
2021-11-24 21:18:44 +0000

    Added options for maxAttemps and Delay

--- models/trigger.py
+++ models/trigger.py
@@ -1,3 +1,5 @@
+import time
+
 from plugins.playbook.models import playbook
 
 import jimi 
@@ -5,10 +7,11 @@ import jimi
 class _playbookSearch(jimi.trigger._trigger):
     playbookName = str()
     sequence = int()
-    inComplete = bool()
-    excludeIncrementSequence = bool()
+    inComplete = False
+    excludeIncrementSequence = True
     playbookLimit = 5
-    excludeMaxAttempts = True
+    maxAttempts = int()
+    delayBetweenAttempts = int()
 
     def check(self):
         if self.excludeIncrementSequence:
@@ -54,6 +57,10 @@ class _playbookSearch(jimi.trigger._trigger):
             ]
             for field in playbook.playbokFields:
                 aggregateStatement[5]["$project"][field] = "$doc.{0}".format(field)
+            if self.maxAttempts:
+                aggregateStatement[4]["$match"]["doc.maxAttempts"] = { "$lte" : self.maxAttempts }
+            if self.delayBetweenAttempts:
+                aggregateStatement[4]["$match"]["doc.startTime"] = { "$lt" : time.time() - self.delayBetweenAttempts }
             playbooks = playbook._playbook().aggregate(aggregateStatement=aggregateStatement,limit=self.playbookLimit)
         else:
             playbooks = playbook._playbook().query(query={"name" : self.playbookName, "sequence" : self.sequence, "result" : not self.inComplete },limit=self.playbookLimit,fields=playbook.playbokFields)["results"]
